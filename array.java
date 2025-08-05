dfaf5e39ac07fd0d32c44a796f99fc05baad92b7
Ateeq <mdateequrrahma123@gmail.com>
2025-08-05 18:46:40 +0530

    Add new files

--- array.java
+++ array.java
@@ -22,7 +22,7 @@ public class array {
         System.out.println("The matrix is: ");
         for(int i=0; i<matrix.length; i++) {
             for(int j=0; j<matrix[0].length; j++) {
-                System.out.print(matrix[i][j]+ "      ");
+                System.out.print(matrix[i][j]+ " ");
             }
             System.out.println();
         }
